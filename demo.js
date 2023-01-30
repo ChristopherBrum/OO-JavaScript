fb428ccefcba9536404fcce0f76bd55002ff1a9f
Christopher Brum <christopher.brum@gmail.com>
2023-01-30 13:42:22 -0800

    updated the main.js file

--- demo.js
+++ demo.js
@@ -1,17 +1,9 @@
-// "use strict";
+// "use strict"
 
-foo = 1;
-var moreFoo = 3;
+// function func() {
+//   b = 1;
+// }
 
-function bar() {
-  return 7;
-}
+// func();
 
-delete window.foo;         // deleted
-delete window.moreFoo;     // not deleted
-delete window.bar;         // not deleted
-
-console.log(window.bar());                         // 7
-console.log(window.foo);                           // undefined
-console.log(window.hasOwnProperty("foo"));         // false
-console.log(window.moreFoo);                       // 3
+// console.log(b);
