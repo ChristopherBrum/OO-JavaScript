db0118ed71073e81bdcb2774fe081c3610aff147
Christopher Brum <christopher.brum@gmail.com>
2023-02-21 10:11:48 -0800

    updated demo html and js files

--- demo.js
+++ demo.js
@@ -1,8 +1,27 @@
-function Lizard() {
-  this.scamper = function() {
-    console.log("I'm scampering!");
-  };
+function getDefiningObject(object, propKey) {
+  let lastPrototype = object;
+  let currentPrototype;
+
+  while (currentPrototype !== null) {
+    currentPrototype = Object.getPrototypeOf(lastPrototype);
+
+
+    console.log(Object.getPrototypeOf(Object.prototype));
+    lastPrototype = currentPrototype;
+  }
+
 }
 
-let lizzy = new Lizard();
-lizzy.scamper(); // ?
+let foo = {
+  a: 1,
+  b: 2,
+};
+
+let bar = Object.create(foo);
+let baz = Object.create(bar);
+let qux = Object.create(baz);
+
+bar.c = 3;
+
+console.log(getDefiningObject(qux, 'c') === bar);     // => true
+// console.log(getDefiningObject(qux, 'e'));             // => null
